a0116d0c6c544e43edeccaee7c385617e4833dcd
saccsasse <lehaklass17@gmail.com>
2026-07-17 13:40:54 +0200

    add cart class functionality

---
+++ cart/cart.py
@@ -0,0 +1,15 @@
+class Cart():
+    def __init__(self, request):
+        self.session = request.session
+        cart = request.session.get('cart')
+        if 'cart' not in request.session:
+            cart = self.session['cart'] = {}
+        self.cart = cart
+
+    def add_to_cart(self, product, product_quantity):
+        product_id = product.id
+        if product_id in self.cart:
+            self.cart[product_id]['quantity'] += product_quantity
+        else:
+            self.cart[product_id] = {'price': product.price, 'quantity': product_quantity}
+        self.session.modified = True
--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .cart import Cart
+from myapp.models import Product
+from django.shortcuts import get_object_or_404
 
 def add_to_cart(request):
+    cart = Cart(request)
     if request.method == 'POST':
         product_id = request.POST.get('product_id')
         product_quantity = request.POST.get('product_quantity')
+        print(f"product_id: {product_id}, product_quantity: {product_quantity}")
+        product = get_object_or_404(Product, id=product_id)
+        cart.add_to_cart(product, product_quantity)
     return JsonResponse({'status': 'success'})
